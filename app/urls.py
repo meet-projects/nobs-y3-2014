ae10b8db81dff6139215bc3cb921f77d8d170b84
Guest <guest-HDaSTE@meet.mit.edu>
2014-06-26 16:10:42 +0300

    dummy URLs

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+from app import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.home, name='home'),
+    url(r'^signup/$', views.signup, name='sign up'),
+    url(r'^search_results/$', views.search_results, name='search results'),
+    url(r'^video/$', views.video, name='video'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^upload/$', views.upload, name='upload'),
+)
