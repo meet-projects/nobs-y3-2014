9004d4b0e4c705bf1f8222a5e003cc58dc76b67c
Omri Doron <omri12@meet.mit.edu>
2014-08-11 18:32:43 +0300

    views, models

--- app/models.py
+++ app/models.py
@@ -5,5 +5,17 @@ class Video(models.Model):
     	path = models.CharField(max_length=50)
     	title = models.CharField(max_length=50)
     	description = models.CharField(max_length=100)
-	author = models.ForeignKey(User)
+	uploader = models.ForeignKey(User)
 	date = models.DateField()
+	
+class Profile(models.Model):
+	#def get_code:
+        user = models.OneToOneField(User)
+	email = models.CharField(max_length = 200)
+        name = models.CharField(max_length = 40)
+        userName = models.CharField(max_length = 40)
+        day = models.IntegerField()
+	month = models.IntegerField()
+        year = models.IntegerField(label=u'year')
+	gender = forms.CharField(label=u'gender')
+##        photo = models.ImageField(upload_to = "books/static/images")
