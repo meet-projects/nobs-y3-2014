1859b0271770d725b79f1936449db1231617fd83
Omri Doron <omri12@meet.mit.edu>
2014-08-11 11:52:52 +0300

    views and models changed a bit

--- app/views.py
+++ app/views.py
@@ -5,7 +5,11 @@ def home(request):
 	return render(request, 'app/home.html')
 
 def signup(request):
-    return render(request, 'app/signup.html')
+	context={'months':["January", "February", "March", "April", "May",
+	"June", "July", "August", "September", "October", "November", "December"]}
+	context["days"] = range(1, 32)
+		context["years"] = range(1914, 2007)	
+    return render(request, 'app/signup.html', context)
 
 def search_results(request):
     return render(request, 'app/search_results.html')
