be77a1a3c9e39d558225fdabdef8529bd9ab1854
Ron <ronlipel@gmail.com>
2014-08-11 12:24:48 +0300

    Added video model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Video(models.Model):
+    	path = models.CharField(max_length=50)
+    	title = models.CharField(max_length=50)
+    	description = models.CharField(max_length=100)
+	author = models.ForeignKey(User)
+	date = models.DateField()
